c95072646ae9fc1ba6d442a9563cd55a2fec22fd
Дмитрий Корец <dimon.korecz@gmail.com>
2022-09-22 17:17:19 +0300

    Добавьте файлы проекта.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalExceptionHandlerMiddleware.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        [Route("GetExceptionInfo")]
+        [HttpGet]
+        public IEnumerable<string> GetExceptionInfo()
+        {
+            string[] arrRetValues = null;
+            if (arrRetValues.Length > 0)
+            { }
+            return arrRetValues;
+        }
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ ExceptionHandler/ExceptionHandlerMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace GlobalExceptionHandlerMiddleware.ExceptionHandler
+{
+    public static class ExceptionHandlerMiddlewareExtensions
+    {
+        public static void UseExceptionHandlerMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
